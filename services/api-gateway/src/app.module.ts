864f9f187debfb750f3533bcdfdf3b6c6a776fb5
misat1505 <misat2002@gmail.com>
2026-06-27 12:37:05 +0200

    VIATORA-13 Implemnted caching of the user based on token in api-gateway

--- services/api-gateway/src/app.module.ts
+++ services/api-gateway/src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import KeyvRedis from '@keyv/redis';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: () => ({
+        stores: [
+          new KeyvRedis(
+            `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+          ),
+        ],
+        ttl: 5 * 60 * 1000, // 5 min
+      }),
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
