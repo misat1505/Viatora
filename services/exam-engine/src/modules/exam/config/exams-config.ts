988a9f439762b5228f9ac41b7ee826b68a5dd31c
misat1505 <misat2002@gmail.com>
2026-07-01 13:40:01 +0200

    VIATORA-23 setup configuration for B exam

--- services/exam-engine/src/modules/exam/config/exams-config.ts
+++ services/exam-engine/src/modules/exam/config/exams-config.ts
@@ -1,9 +1,49 @@
-export const EXAMS_CONFIG = {
+type Category = 'B';
+
+enum QuestionType {
+  BASIC = 'basic',
+  SPECIALIST = 'specialist',
+}
+
+type QuestionTypeConfig = {
+  questionType: QuestionType;
+  points: number;
+  count: number;
+};
+
+export const EXAMS_CONFIG: Record<Category, QuestionTypeConfig[]> = {
   B: [
+    // TODO: uncomment that, but for now to reduce sanity calls we will have just 1 request
+
+    // {
+    //   questionType: QuestionType.BASIC,
+    //   points: 3,
+    //   count: 10,
+    // },
+    // {
+    //   questionType: QuestionType.BASIC,
+    //   points: 2,
+    //   count: 6,
+    // },
+    // {
+    //   questionType: QuestionType.BASIC,
+    //   points: 1,
+    //   count: 4,
+    // },
     {
-      questionType: 'specialist',
+      questionType: QuestionType.SPECIALIST,
       points: 3,
-      count: 5,
+      count: 6,
     },
+    // {
+    //   questionType: QuestionType.SPECIALIST,
+    //   points: 2,
+    //   count: 4,
+    // },
+    // {
+    //   questionType: QuestionType.SPECIALIST,
+    //   points: 1,
+    //   count: 2,
+    // },
   ],
 } as const;
