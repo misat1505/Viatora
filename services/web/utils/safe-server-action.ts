9025b5d0732caedb76a33dfdae042025677e9d41
misat1505 <misat2002@gmail.com>
2026-07-02 13:22:41 +0200

    VIATORA-28 Created an endpoint for getting session by id and created dummy web page

--- services/web/utils/safe-server-action.ts
+++ services/web/utils/safe-server-action.ts
@@ -9,10 +9,10 @@ import {
 
 export type ServerActionResponse<T> = [AppError, null] | [null, T];
 
-export function safeServerAction<T>(fn: () => Promise<T>) {
-  return async (): Promise<ServerActionResponse<T>> => {
+export function safeServerAction<Args extends unknown[], T>(fn: (...args: Args) => Promise<T>) {
+  return async (...args: Args): Promise<ServerActionResponse<T>> => {
     try {
-      const data = await fn();
+      const data = await fn(...args);
       return [null, data];
     } catch (e) {
       if (e instanceof AxiosError) {
@@ -21,16 +21,12 @@ export function safeServerAction<T>(fn: () => Promise<T>) {
         switch (status) {
           case 400:
             return [new BadRequestError(e.message), null];
-
           case 401:
             return [new UnauthorizedError(), null];
-
           case 404:
             return [new NotFoundError(), null];
-
           case 500:
             return [new InternalServerError(), null];
-
           default:
             return [new AppError(e.message), null];
         }
