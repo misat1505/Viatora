6e2f73b2312f836ca71dfe24636eff33a9616c31
misat1505 <misat2002@gmail.com>
2026-07-02 14:05:27 +0200

    VIATORA-27 Added dark mode to the app

--- services/web/providers/index.tsx
+++ services/web/providers/index.tsx
@@ -1,10 +1,15 @@
 import { PropsWithChildren } from 'react';
 import RefreshTokenProvider from './refresh-token-provider';
+import { ThemeProvider } from './theme-provider';
 
 type ProvidersProps = PropsWithChildren;
 
 const Providers = ({ children }: ProvidersProps) => {
-  return <RefreshTokenProvider>{children}</RefreshTokenProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <RefreshTokenProvider>{children}</RefreshTokenProvider>
+    </ThemeProvider>
+  );
 };
 
 export default Providers;
