6a7b05296bd1624a9830b1cf89386d35f068fff3
misat1505 <misat2002@gmail.com>
2026-07-12 13:52:39 +0200

    VIATORA-54 improved error messages on unuathorized on plans page, introduced sonner

--- services/web/app/[lang]/layout.tsx
+++ services/web/app/[lang]/layout.tsx
@@ -4,6 +4,7 @@ import LocaleProvider from '@/providers/locale-provider';
 import { getServerPalette } from '@/lib/get-server-pallette';
 import { Navbar } from '@/components/navbar';
 import { Footer } from '@/components/footer';
+import { Toaster } from '@/components/ui/sonner';
 
 export async function generateStaticParams() {
   return [{ lang: 'pl' }, { lang: 'en' }];
@@ -19,6 +20,7 @@ export default async function LangLayout({ children, params }: LayoutProps<'/[la
         <Navbar lang={lang} />
         <main className="min-h-[calc(100vh-4rem)]">{children}</main>
         <Footer />
+        <Toaster />
       </Providers>
     </LocaleProvider>
   );
--- services/web/components/pricing/checkout-button.tsx
+++ services/web/components/pricing/checkout-button.tsx
@@ -4,6 +4,9 @@ import { PropsWithChildren } from 'react';
 import { Button } from '../ui/button';
 import { CreateCheckoutDTO } from '@/generated/viatoraAPI.schemas';
 import { checkoutPlan } from '@/actions/payments/checkout-plan';
+import { toast } from 'sonner';
+import GoogleOAuthLink from '../google-oauth-link';
+import LocaleText from '../locale-text';
 
 type CheckoutButtonProps = PropsWithChildren & {
   isPopular: boolean;
@@ -14,6 +17,13 @@ type CheckoutButtonProps = PropsWithChildren & {
 const CheckoutButton = ({ children, isPopular, category, months }: CheckoutButtonProps) => {
   async function handleClick(dto: CreateCheckoutDTO) {
     const [error, result] = await checkoutPlan(dto);
+    if (error?.message === 'Unauthorized') {
+      return toast.error(
+        <GoogleOAuthLink className="[div:has(>div>&)]:w-full w-full">
+          <LocaleText k="auth.loginRequired.googleButton" />
+        </GoogleOAuthLink>,
+      );
+    }
     if (error) throw error;
 
     window.location.href = result.checkoutUrl;
