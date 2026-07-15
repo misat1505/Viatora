df24b57cef5d449906c52bb69c1c44cfd43414b3
misat1505 <misat2002@gmail.com>
2026-07-15 17:28:00 +0200

    VIATORA-56 added mypy support for stats service

--- services/statistics-service/features/stats/stats_servicer.py
+++ services/statistics-service/features/stats/stats_servicer.py
@@ -12,6 +12,5 @@ class StatsServicer(stats_pb2_grpc.StatsServiceServicer):
 
     @ValidateRequest(GetSummaryRequest)
     async def GetSummary(self, request: GetSummaryRequest, _: grpc.ServicerContext):
-        print(request)
         data = await self.stats_service.get_summary(request)
         return stats_pb2.GetSummaryResponse(**data.model_dump())  # type: ignore[attr-defined]
