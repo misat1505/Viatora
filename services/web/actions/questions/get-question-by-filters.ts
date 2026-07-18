1cae5ea4052fcfc31791be7fb55f955e25539412
misat1505 <misat2002@gmail.com>
2026-07-18 21:41:31 +0200

    VIATORA-64 fixed build in web app

--- services/web/actions/questions/get-question-by-filters.ts
+++ services/web/actions/questions/get-question-by-filters.ts
@@ -1,11 +1,11 @@
 'use server';
 
-import { GetQuestionsQueryDto } from '@/generated/viatoraAPI.schemas';
+import { GetQuestionsQueryDTO } from '@/generated/viatoraAPI.schemas';
 import { QuestionsControllerGetQuestionsResponse } from '@/generated/zod/questions/questions';
 import { questionsApiClient } from '@/lib/api';
 import { safeServerAction } from '@/utils/safe-server-action';
 
-export const getQuestionByFilters = safeServerAction(async (dto: GetQuestionsQueryDto) => {
+export const getQuestionByFilters = safeServerAction(async (dto: GetQuestionsQueryDTO) => {
   const response = await questionsApiClient.questionsControllerGetQuestions(dto);
   return QuestionsControllerGetQuestionsResponse.parse(response.data);
 });
